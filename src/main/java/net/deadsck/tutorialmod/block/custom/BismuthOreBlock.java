6d3e0bdb9638c9b14567457026abcbd0375c33e2
Mathis <mathis.eddam86@gmail.com>
2025-07-21 20:28:39 +0200

    First Blocks, Items, Recipes

---
+++ src/main/java/net/deadsck/tutorialmod/block/custom/BismuthOreBlock.java
@@ -0,0 +1,24 @@
+package net.deadsck.tutorialmod.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+
+public class BismuthOreBlock extends DropExperienceBlock {
+
+    public BismuthOreBlock(IntProvider xpRange, Properties properties) {
+        super(xpRange, properties);
+    }
+
+    @Override
+    public boolean onDestroyedByPlayer(BlockState state, Level level, BlockPos pos, Player player, boolean willHarvest, FluidState fluid) {
+        level.playSound(null, pos, SoundEvents.SMALL_AMETHYST_BUD_BREAK, SoundSource.BLOCKS);
+        return super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid);
+    }
+}
